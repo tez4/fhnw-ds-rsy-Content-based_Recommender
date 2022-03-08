4b3596dfceb80c62ebbc28bd28bec7a1f6b7f3a7
tez4 <joel.twann@bluewin.ch>
2022-03-08 21:04:14 +0100

    Made multiple changes and added text

--- helper.R
+++ helper.R
@@ -53,21 +53,49 @@ plot_similarity <- function(df, sub_title) {
 # ------------------- cleveland dot plot ---------------------------
 
 
-create_cleveland_plot_1 <- function(df, title) {
-  ggplot(df, aes(user, rating), height = 500, width = 7) +
-    scale_color_discrete(labels = c("bestbewertete Filme", "Top-N Empfehlungen")) +
+create_cleveland_plot <- function(rating_matrix, genres, n, our_user) {
+  
+  recommendation <- get_topn_recos(rating_matrix, n)
+  recommendation <- recommendation %>% filter(user == our_user)
+  
+  genres_df <- cbind(item = rownames(genres),genres)
+  
+  # bring recommendations into the right form
+  recommendation <- inner_join(recommendation, genres_df, by = 'item')
+  recommendation <- pivot_longer(recommendation, cols = c('unknown': 'Western'), names_to = 'genre', values_to = 'genre_value')
+  recommendation <- recommendation %>% group_by(user, genre) %>% summarise(recommendation = sum(genre_value))
+  recommendation <- inner_join(recommendation, recommendation %>% group_by(user) %>% summarize(total = sum(recommendation)), by = 'user')
+  recommendation <- recommendation %>% mutate(recommendation = (recommendation / total) * 100)
+  
+  # bring the actually seen movies into the right form
+  movies <- as(MovieLense, "data.frame")
+  movies <- movies %>% filter(user == our_user)
+  movies <- inner_join(movies, genres_df, by = 'item')
+  movies <- pivot_longer(movies, cols = c('unknown': 'Western'), names_to = 'genre', values_to = 'genre_value')
+  movies <- movies %>% group_by(user, genre) %>% summarise(actual = sum(genre_value))
+  movies <- inner_join(movies, movies %>% group_by(user) %>% summarize(total = sum(actual)), by = 'user')
+  movies <- movies %>% mutate(actual = (actual / total) * 100)
+  
+  # join the two datasets
+  reco_df <- inner_join(movies, recommendation, by = c('user', 'genre'))
+  reco_df <- reco_df %>% mutate(act = actual)
+  reco_df <- pivot_longer(reco_df, cols = c('actual', 'recommendation'), names_to = 'type', values_to = 'value')
+  
+  reco_df <- reco_df %>% filter(user == our_user)
+  
+  ggplot(reco_df, aes(reorder(genre, act), value), height = 500, width = 7) +
+    scale_color_discrete(labels = c("bewertete Filme", "Top-20 Empfehlungen")) +
     coord_flip() +
     geom_line() +
-    geom_point(aes(color = user)) +
+    geom_point(aes(color = type)) +
     theme_minimal() +
     labs(
-      title = title,
+      title = glue('Top-{n} Empfehlungen für Nutzer {our_user}'),
       x = element_blank(),
-      y = "Rating (1 = gut, 0 = schlecht)",
+      y = "Anteil in Prozent",
       color = element_blank()
     ) +
     theme(
-      text = element_text(size = 12),
-      legend.position = 'none'
+      legend.position = 'bottom'
     )
 }
